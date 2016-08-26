c333b21a54b91e23de3f15fdb40d0dae12b2c745
SrishtiR <srishti.rawal2012@vit.ac.in>
2016-08-26 17:34:54 +0530

    Update FixedPointInArray.java

--- Arrays/FixedPointInArray.java
+++ Arrays/FixedPointInArray.java
@@ -1,1 +1,30 @@
 
+package com.srishti.DSA;
+
+public class FixedPointInArray {
+	/* Find a Fixed Point in a given array -
+	 * Linear Search
+	 * O(n)*/
+	public int fixedPoint(int[] arr, int size) {
+		for(int i=0;i<size;i++) {
+			if(i==arr[i]){
+				return i;
+			}
+		}
+		return -1;
+	}
+	/* Find a Fixed Point in a given array -
+	 * Binary Search
+	 * O(log(n)) */
+	public int fixedPointUsingBinarySearch(int[] arr,int low,int high){
+		int mid = (low+high)/2;
+		if(mid == arr[mid]){
+			return mid;
+		}
+		if(mid > arr[mid]) {
+			return fixedPointUsingBinarySearch(arr,(mid+1),high);
+		} else {
+			return fixedPointUsingBinarySearch(arr,low,mid-1);
+		}
+	}
+}
