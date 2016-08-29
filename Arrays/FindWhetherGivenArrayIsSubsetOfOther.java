dbd3a5d6572681c4fcb96f58911d24bf41395ecb
SrishtiR <srishti.rawal2012@vit.ac.in>
2016-08-29 15:03:27 +0530

    Update FindWhetherGivenArrayIsSubsetOfOther.java

--- Arrays/FindWhetherGivenArrayIsSubsetOfOther.java
+++ Arrays/FindWhetherGivenArrayIsSubsetOfOther.java
@@ -14,4 +14,28 @@ public class FindWhetherGivenArrayIsSubsetOfOther {
 		}
 		return true;
 	}
+	/* 51. Find whether an array is subset of another array 
+	 * using sorting and merging 
+	 * works in case of repetitions */
+	public boolean findWhetherArrayIsSubsetOfAnotherSS(int[] arr1, int[] arr2, int size1, int size2){
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		int i=0,j=0;
+		while(i<size1 && j<size2){
+			if(arr1[i]<arr2[j]){
+				i++;
+			}
+			else if(arr1[i]==arr2[j]){
+				i++;
+				j++;
+			} else if(arr1[i] > arr2[j]) {
+				return false;
+			}
+			
+		}
+		if(j<size2)
+			return false;
+		else
+			return true;
+	}
 }
