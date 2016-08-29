8d039bf8b0adeeb925b6b544a83e880bfa389f41
SrishtiR <srishti.rawal2012@vit.ac.in>
2016-08-29 14:57:42 +0530

    Create FindSmallestMissingNumber.java

---
+++ Arrays/FindSmallestMissingNumber.java
@@ -0,0 +1,16 @@
+public class FindSmallestMissingNumber {
+	int findFirstMissing(int[] array,int start, int end){
+		if(start>end){
+			return end+1;
+		}
+		if(start != array[start]) {
+			return start;
+		}
+		int mid = (start+end)/2;
+		if(array[mid]>mid){
+			return findFirstMissing(array,start,mid);
+		} else {
+			return findFirstMissing(array,mid+1,end);
+		}
+	}
+}
